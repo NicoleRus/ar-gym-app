4a1042d6a6b2e62f838953f7878af64da735315d
Nicole Berreth <nicoler.russo12@gmail.com>
2025-04-24 19:23:36 -0500

    refactor(top-nav): center app title

--- lib/widgets/top_nav.dart
+++ lib/widgets/top_nav.dart
@@ -36,67 +36,70 @@ class VAppBarDefault extends StatelessWidget implements PreferredSizeWidget {
           onTap: () => Scaffold.of(context).openDrawer(),
         ),
       ),
-      title: Center(
-        child: Semantics(
-          label: 'AR Fitness Logo',
-          child: Image.asset(
-            'assets/ARFitnessLogoFinal_Red-Bk.png',
-            height: 32,
-            fit: BoxFit.contain,
-          ),
-        ),
-      ),
-      actionList: [
-        // Padding(
-        //   padding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
-        //   child: Semantics(
-        //     label: "Search",
-        //     button: true,
-        //     child: InkWell(
-        //       customBorder: const CircleBorder(),
-        //       splashColor: VColors.defaultSurfaceLowlight,
-        //       child: Container(
-        //         width: 44,
-        //         height: 44,
-        //         padding: const EdgeInsets.all(10),
-        //         child: const ExcludeSemantics(
-        //           child: VIcon(
-        //             svgIcon: VIcons.searchLow,
-        //             iconHeight: 24,
-        //             iconWidth: 24,
-        //           ),
-        //         ),
-        //       ),
-        //       onTap: () {},
-        //     ),
-        //   ),
-        // ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(4, 4, 16, 4),
-          child: Semantics(
-            label: "Profile",
-            button: true,
-            child: InkWell(
-              customBorder: const CircleBorder(),
-              splashColor: VColors.defaultSurfaceLowlight,
-              child: Container(
-                width: 44,
-                height: 44,
-                padding: const EdgeInsets.all(10),
-                child: const ExcludeSemantics(
-                  child: VIcon(
-                    svgIcon: VIcons.accountLow,
-                    iconColor: VColors.defaultActive,
-                    iconHeight: 24,
-                    iconWidth: 24,
-                  ),
-                ),
-              ),
-              onTap: () {},
+      title: Stack(
+        alignment: Alignment.center,
+        children: [
+          Semantics(
+            label: 'AR Fitness Logo',
+            child: Image.asset(
+              'assets/ARFitnessLogoFinal_Red-Bk.png',
+              height: 32,
+              fit: BoxFit.contain,
             ),
           ),
-        ),
-      ],
+        ],
+      ),
+      // actionList: [
+      //   Padding(
+      //     padding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
+      //     child: Semantics(
+      //       label: "Search",
+      //       button: true,
+      //       child: InkWell(
+      //         customBorder: const CircleBorder(),
+      //         splashColor: VColors.defaultSurfaceLowlight,
+      //         child: Container(
+      //           width: 44,
+      //           height: 44,
+      //           padding: const EdgeInsets.all(10),
+      //           child: const ExcludeSemantics(
+      //             child: VIcon(
+      //               svgIcon: VIcons.searchLow,
+      //               iconHeight: 24,
+      //               iconWidth: 24,
+      //             ),
+      //           ),
+      //         ),
+      //         onTap: () {},
+      //       ),
+      //     ),
+      //   ),
+      //   Padding(
+      //     padding: const EdgeInsets.fromLTRB(4, 4, 16, 4),
+      //     child: Semantics(
+      //       label: "Profile",
+      //       button: true,
+      //       child: InkWell(
+      //         customBorder: const CircleBorder(),
+      //         splashColor: VColors.defaultSurfaceLowlight,
+      //         child: Container(
+      //           width: 44,
+      //           height: 44,
+      //           padding: const EdgeInsets.all(10),
+      //           child: const ExcludeSemantics(
+      //             child: VIcon(
+      //               svgIcon: VIcons.accountLow,
+      //               iconColor: VColors.defaultActive,
+      //               iconHeight: 24,
+      //               iconWidth: 24,
+      //             ),
+      //           ),
+      //         ),
+      //         onTap: () {},
+      //       ),
+      //     ),
+      //   ),
+      // ],
     );
   }
 }
